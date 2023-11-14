e085000ee5b37630bfd8eddd889ee5b410b1f5c3
vladboychev <vladboychev@gmail.com>
2023-11-14 15:18:51 +0200

    tme beforeEach

---
+++ 006/tme/sample/test/forEach.test.js
@@ -0,0 +1,23 @@
+const assert = require("assert");
+
+const { forEach } = require("../index");
+
+let numbers;
+
+beforeEach(() => {
+  numbers = [1, 2, 3];
+});
+
+it("Should sum an array..", () => {
+  let total = 0;
+
+  forEach(numbers, (value) => {
+    total += value;
+  });
+
+  assert.strictEqual(total, 6);
+});
+
+it("beforeEach is ran each time..", () => {
+  assert.strictEqual(numbers.length, 4);
+});
